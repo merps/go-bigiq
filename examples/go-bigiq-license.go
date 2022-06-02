8371bca3f2ae051724303d97c6001340eacc3947
Michael Kennedy <merpso@gmail.com>
2022-06-02 15:25:51 +1000

    EULA time!!!

--- examples/go-bigiq-license.go
+++ examples/go-bigiq-license.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	// Connect to the BIQ-IP system. Enabled basic auth in BIQ - https://support.f5.com/csp/article/K43725273
 	// Correct by adding unknown type var - really!?
-	f5, _ := bigiq.NewTokenSession("10.0.90.253", "443", "admin", "SuperSecrect", "tmos", nil)
+	f5, _ := bigiq.NewTokenSession("10.0.90.253", "443", "admin", "zun.lull-PLEW7ar", "tmos", nil)
 
 	//Get Licenses
 	//licenses, err := f5.GetRegPools()
@@ -41,6 +41,12 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	eula := resRef["eulaText"].(string)
 	response := resRef["status"].(string)
-	fmt.Println(response)
+
+	fmt.Printf("License Status: %s\n", response)
+	fmt.Println()
+	//TODO: need to capture and feedback EULA as patch operation,
+	//reference: https://clouddocs.f5.com/products/big-iq/mgmt-api/v8.1.0/HowToSamples/bigiq_public_api_wf/t_license_initial_activation.html
+	fmt.Println(eula)
 }
