1283ea82e78c7d12d71680aea26a459a2521e11f
Michael Kennedy <merpso@gmail.com>
2022-06-02 15:16:27 +1000

    EULA time!!!

--- examples/go-bigiq-license.go
+++ examples/go-bigiq-license.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	// Connect to the BIQ-IP system. Enabled basic auth in BIQ - https://support.f5.com/csp/article/K43725273
 	// Correct by adding unknown type var - really!?
-	f5, _ := bigiq.NewTokenSession("10.0.90.253", "443", "admin", "SuperSecret", "tmos", nil)
+	f5, _ := bigiq.NewTokenSession("10.0.90.253", "443", "admin", "SuperSecrect", "tmos", nil)
 
 	//Get Licenses
 	//licenses, err := f5.GetRegPools()
@@ -36,11 +36,11 @@ func main() {
 	//fmt.Println(response)
 
 	//Poll for status TODO: does this need json marshalling?
-	response, err := f5.PollActivation("OTCCU-KKAYN-KZXPF-VXKAM-SQQZCFH")
+	resRef, err := f5.PollActivation("OTCCU-KKAYN-KZXPF-VXKAM-SQQZCFH")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	response := resRef["status"].(string)
 	fmt.Println(response)
-
 }
